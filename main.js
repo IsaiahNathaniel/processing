f7e8a7cd2954cb77f4dceea7fa64136b3e3174c8
Isaiah <isaiah712.minniear@gmail.com>
2017-03-13 22:52:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var pi_chudnovsky_bs = function(digits){
     var C = 640320;
     var C3_OVER_24 = floor(pow(C,3)/24);
     var bs = function(a, b){
-        /*
+        
         Computes the terms for binary splitting the Chudnovsky infinite series
 
         a(a) = +/- (13591409 + 545140134*a)
@@ -12,7 +12,7 @@ var pi_chudnovsky_bs = function(digits){
         q(a) = a*a*a*C3_OVER_24
 
         returns P(a,b), Q(a,b) and T(a,b)
-        */
+        
         var Pab, Qab, Tab;
         if(b - a === 1){
             // Directly compute P(a,a+1), Q(a,a+1) and T(a,a+1)
@@ -50,7 +50,7 @@ var pi_chudnovsky_bs = function(digits){
     var DIGITS_PER_TERM = log(C3_OVER_24/6/2/6);
     var N = digits/DIGITS_PER_TERM + 1;
     // Calclate P(0,N) and Q(0,N)
-    var P= setTimeout(bs(0, N), 0), Q= setTimeout(bs(0, N), 0), T = setTimeout(bs(0, N), 0);             /ONE MORE LINE OF RECURSIVE.
+    var P = setTimeout(bs(0, N), 0), Q= setTimeout(bs(0, N), 0), T = setTimeout(bs(0, N), 0);             /ONE MORE LINE OF RECURSIVE.
     var one = pow(10,digits);
     var sqrtC = sqrt(10005*one);
     return floor((Q*426880*sqrtC) / T);
